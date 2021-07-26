7b8160eebdb12dc6c0ae2647724cf01b41067956
laffed <roarklaffed@gmail.com>
2021-07-26 14:23:37 -0500

    updated

--- src/scenes/index.ts
+++ src/scenes/index.ts
@@ -2,16 +2,10 @@ import SignIn from "./SignIn/SignIn";
 import SignUp from "./Signup/Signup";
 import Home from "./Home/Home";
 import Onboarding from "./Onboarding/Onboarding";
-import MainMenu from "./MainMenu/MainMenu";
-import HomeSearch from "./HomeSearch/HomeSearch";
-import ProductDetail from "./ProductDetail/ProductDetail";
 
 export {
-	SignIn,
-	SignUp,
-	Home,
-	Onboarding,
-	MainMenu,
-	HomeSearch,
-	ProductDetail
+  SignIn,
+  SignUp,
+  Home,
+  Onboarding,
 };
